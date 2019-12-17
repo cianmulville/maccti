fae296423b281f2cb6bd596362635257c917f7c1
Cian Mulville <cian@dataflowinternational.com>
2019-12-17 17:47:11 +0000

    post loop

--- index.php
+++ index.php
@@ -65,7 +65,12 @@
     <main>
         
         <div class="home-banner bg-light">
-            <?php the_content(); ?>
+            <?php if(have_posts()) {
+                while(have_posts()) {
+                    the_post();
+                    the_content();
+                }
+            } ?>
 
         </div>
     </main>
