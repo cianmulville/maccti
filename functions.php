59dfc52757f3d3b194f4f92792ca96861c4785bc
Cian Mulville <cian@dataflowinternational.com>
2020-01-10 15:15:13 +0000

    single quotes on stylesheet strings

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@ https://use.typekit.net/twt3hey.css
 
 function add_maccti_scripts() {
     wp_enqueue_style('proxima_nova', 'https://use.typekit.net/twt3hey.css');
-    wp_enqueue_style('font_awesome', get_template_directory_uri() . `/font-awesome/css/fontawesome.min.css`);
-    wp_enqueue_style('bootstrap', get_template_directory_uri() . `/css/bootstrap.min.css`, array('font_awesome'));
+    wp_enqueue_style('font_awesome', get_template_directory_uri() . '/font-awesome/css/fontawesome.min.css');
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array('font_awesome'));
     wp_enqueue_style('style', get_stylesheet_uri(), array('bootstrap', 'font_awesome', 'proxima_nova'));
 
     wp_enqueue_script( 'popper', get_template_directory_uri() . '/js/popper.min.js', array ( 'jquery' ), 1.1, true);
