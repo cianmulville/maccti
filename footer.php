7f2b85a885dbefd7eae7e15c70ec0622a3edca5a
Cian Mulville <cian@dataflowinternational.com>
2020-01-10 15:18:25 +0000

    font awesome version patch

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <footer>
         <div class="footer-banner">
             <p>Follow our posts at:
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-facebook-square"></i>
-                <i class="fab fa-instagram"></i>
+                <i class="fa fa-twitter"></i>
+                <i class="fa fa-facebook-square"></i>
+                <i class="fa fa-instagram"></i>
             </p>
         </div>
         <!-- <div class="maccti-flex-row justify-content-center pt-50 payment-info">
-            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/SSL.png" alt="SSL">
-            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/betalen-met-paypal-vloerkledenwebshopnl.png" alt="Paypal">
-            <img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/images/credit_card_logos-2010-horiz1_copy.png" alt="Credit cards">
+            <img class="img-responsive" src=">/images/SSL.png" alt="SSL">
+            <img class="img-responsive" src="/images/betalen-met-paypal-vloerkledenwebshopnl.png" alt="Paypal">
+            <img class="img-responsive" src="/images/credit_card_logos-2010-horiz1_copy.png" alt="Credit cards">
         </div> -->
     </footer>
 <?php wp_footer(); ?>
