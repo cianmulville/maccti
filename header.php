efa8777bb0624e5940076733f7881ad4d2811609
Cian Mulville <cian@dataflowinternational.com>
2020-01-15 18:41:11 +0000

    updated title meta

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo '' . the_title() . ' - ' . bloginfo('name'); ?></title>
+    <title><?php the_title(); ?> | <?php bloginfo('name'); ?></title>
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/favicon-16x16.png">
